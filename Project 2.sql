df3ff64154bc08edaa182d8ad03d141b3258da91
aaeilts <60226103+aaeilts@users.noreply.github.com>
2021-04-21 21:12:03 -0500

    Populated
    Created with all constraints and populated.

--- Project 2.sql
+++ Project 2.sql
@@ -1,16 +1,17 @@
-CREATE DATABASE IF NOT EXISTS Project2;
+DROP SCHEMA IF EXISTS Project2;
+CREATE DATABASE Project2;
 USE Project2;
 SET SQL_SAFE_UPDATES = 0;
 
 CREATE TABLE IF NOT EXISTS Advertisements
-(Advertisement_ID SMALLINT unsigned,
+(Advertisement_ID SMALLINT UNSIGNED AUTO_INCREMENT,
 Adv_Title VARCHAR(30),
 Adv_Deets VARCHAR(100),
 Adv_DateTime DATETIME, 
-Price FLOAT(5,2),
+Price DOUBLE(7,2),
+Category_ID VARCHAR(15),
 User_ID VARCHAR(15),
 Moderator_ID VARCHAR(15),
-Category_ID VARCHAR(15),
 Status_ID VARCHAR(2),
 
 CONSTRAINT PK_ADVERT_ID PRIMARY KEY (Advertisement_ID),
@@ -20,6 +21,16 @@ CONSTRAINT FK_CAT_ID FOREIGN KEY (Category_ID) REFERENCES Categories (Category_I
 CONSTRAINT FK_STAT_ID FOREIGN KEY (Status_ID) REFERENCES Statuses (Status_ID) ON DELETE RESTRICT
 );
 
+INSERT INTO Advertisements
+(Adv_Title, Adv_Deets, Adv_DateTime, Price, Category_ID, User_ID, Moderator_ID, Status_ID)
+VALUES 
+("2010 Sedan Subaru", "2010 sedan car in great shape for sale", "2017-02-10", 6000, "CAT", "Rkale", "Jsmith", "AC"),
+("Nice Office Desk", "Nice office desk for sale", "2017-02-15", "50.25", "HOU", "Rkale", "Jsmith", "AC"),
+("Smart LG TV for $200 ONLY", "Smart LG TV 52 inches! Really cheap!", "2017-03-15", "200", "ELC", "Sali", "Jsmith", "AC"),
+("HD Tablet for $25 only", "Amazon Fire Tablet HD", "2017-03-20", "25", "ELC", "Rkale", NULL, "PN"),
+("Laptop for $100", "Amazing HP laptop for $100", "2017-03-20", "100", "ELC", "Rkale", NULL, "PN");
+
+
 CREATE TABLE IF NOT EXISTS Categories
 (Category_ID VARCHAR (3),
 Cat_Name VARCHAR(20),
@@ -27,12 +38,18 @@ Cat_Name VARCHAR(20),
 CONSTRAINT PK_CAT_ID PRIMARY KEY (Category_ID)
 );
 
+INSERT INTO Categories (Category_ID, Cat_Name)
+VALUES ("CAT", "Cars and Trucks"), ("HOU", "Housing"), ("ELC", "Electronics"), ("CCA", "Child Care");
+
 CREATE TABLE IF NOT EXISTS Statuses
 (Status_ID VARCHAR(2),
 Status_Name VARCHAR(15),
 
 CONSTRAINT PK_STAT_ID PRIMARY KEY (Status_ID)
 );
+
+INSERT INTO Statuses (Status_ID, Status_Name)
+VALUES ("PN", "Pending"), ("AC", "Active"), ("DI", "Disapprove");
 
 CREATE TABLE IF NOT EXISTS Users
 (User_ID VARCHAR (15),
@@ -42,6 +59,11 @@ UserLastName VARCHAR(15),
 CONSTRAINT PK_USER_ID PRIMARY KEY (User_ID)
 );
 
+INSERT INTO Users (User_ID, UserFirstName, UserLastName)
+Values 
+("Jsmith", "John", "Smith"), ("Ajackson", "Ann", "Jackson"),
+("Rkale", "Rania", "Kale"), ("Sali", "Samir", "Ali");
+
 CREATE TABLE IF NOT EXISTS Moderators
 (User_ID VARCHAR (15),
 
@@ -49,4 +71,5 @@ CONSTRAINT PK_MOD_ID PRIMARY KEY (User_ID),
 CONSTRAINT FK_MOD_ID FOREIGN KEY (User_ID) REFERENCES Users (User_ID) ON DELETE RESTRICT
 );
 
-
+INSERT INTO Moderators (User_ID)
+VALUES ("Jsmith"), ("Ajackson");
